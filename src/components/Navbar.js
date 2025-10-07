07dc97cc3617b778d20d2da2a1b1fec0670c0c2d
Kandekore <darren@kandekore.net>
2025-10-07 21:28:13 +0100

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+// Step 1: Import your logo image
+import logo from '../assets/images/logo.png'; // Make sure this path is correct
 
 const AppNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand as={Link} to="/">SalvageCar</Navbar.Brand>
+        {/* Step 2: Use the imported logo in the Navbar.Brand */}
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={logo}
+            height="100" // Adjust height as needed
+            className="d-inline-block align-top"
+            alt="SalvageCar Logo"
+          />
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/mot-failures">MOT Failures</Nav.Link>
-            {/* Add the new link here */}
             <Nav.Link as={Link} to="/mechanical-failure">Mechanical Failure</Nav.Link>
             <Nav.Link as={Link} to="/insurance-write-off">Insurance Write Off</Nav.Link>
             <Nav.Link as={Link} to="/accident-damage">Accident Damage</Nav.Link>
